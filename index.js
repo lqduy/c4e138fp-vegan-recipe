79d5dffef366dc02c053b1a3449295a850fffc68
lqduycp <lqduycp@gmail.com>
2023-05-24 00:17:07 +0700

    upload

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-import { recipeCollectionSpread } from "./database/database-recipes.js";
-import { render1RecipeBox, renderRecipeTagsAll } from "./function/render-recipebox.js";
-import { addRecipeToMyCollection } from "./function/cart-and-collection.js";
+import { renderUserSpace, renderTheCorner } from './reuse/script-reuse.js';
+import { makeSignUpLogInBtn, makeSubmitSignUpLogInBtn } from '../function/signup-login-logout.js';
+import { recipeCollectionSpread } from './database/database-recipes.js';
+import { render1RecipeBox, renderRecipeTagsAll } from './function/render-recipebox.js';
+import { addRecipeToMyCollection } from './function/cart-and-collection.js';
 
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('./reuse/form-signup-login.html')
+        .then((response) => response.text())
+        .then((data) => {
+            document.querySelector('.form-signup-login').innerHTML = data;
+            makeSignUpLogInBtn();
+            makeSubmitSignUpLogInBtn();
+        });
+    fetch('./reuse/header.html')
+        .then((response) => response.text())
+        .then((data) => {
+            document.querySelector('header').innerHTML = data;
+            renderUserSpace();
+        });
+    fetch('./reuse/footer.html')
+        .then((response) => response.text())
+        .then((data) => {
+            document.querySelector('footer').innerHTML = data;
+        });
+    renderTheCorner();
+});
 
 // Lấy số ngẫu nhiên
 function getRandomNumbers(arr, n) {
@@ -22,7 +45,6 @@ function getRandomNumbers(arr, n) {
 
 // Lấy ngẫu nhiên 4 công thức gợi ý trong danh sách phẳng
 const getRandomRecipes = getRandomNumbers(recipeCollectionSpread(), 4).map((value) => recipeCollectionSpread()[value]);
-
 
 // Render list công thức gợi ý trên Trang chủ
 
