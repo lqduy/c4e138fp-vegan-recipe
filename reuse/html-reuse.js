48f4371a2df5ecb3ae9d146dd9980437e705c19d
lqduycp <lqduycp@gmail.com>
2023-05-24 12:24:35 +0700

    upload

--- reuse/html-reuse.js
+++ reuse/html-reuse.js
@@ -1,21 +1,21 @@
-import { renderUserSpace, renderTheCorner } from '../reuse/script-reuse.js';
-import { makeSignUpLogInBtn, makeSubmitSignUpLogInBtn } from '../function/signup-login-logout.js';
+import { renderUserSpace, renderTheCorner } from '/reuse/script-reuse.js';
+import { makeSignUpLogInBtn, makeSubmitSignUpLogInBtn } from '/function/signup-login-logout.js';
 
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('../reuse/form-signup-login.html')
+    fetch('/reuse/form-signup-login.html')
         .then((response) => response.text())
         .then((data) => {
             document.querySelector('.form-signup-login').innerHTML = data;
             makeSignUpLogInBtn();
             makeSubmitSignUpLogInBtn();
         });
-    fetch('../reuse/header.html')
+    fetch('/reuse/header.html')
         .then((response) => response.text())
         .then((data) => {
             document.querySelector('header').innerHTML = data;
             renderUserSpace();
         });
-    fetch('../reuse/footer.html')
+    fetch('/reuse/footer.html')
         .then((response) => response.text())
         .then((data) => {
             document.querySelector('footer').innerHTML = data;
