9ab8dd966ac0a24d3b99fdd2dd38ada18a21124d
lqduycp <lqduycp@gmail.com>
2023-05-26 11:12:55 +0700

    upload2605

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-import { recipeCollectionSpread } from '../database/database-recipes.js';
-import { render1RecipeBox, renderRecipeTagsAll } from '../function/render-recipebox.js';
-import { addRecipeToMyCollection } from '../function/cart-and-collection.js';
+import { recipeCollectionSpread } from './database/database-recipes.js';
+import { chefsList } from './database/database-chefs.js';
+import { render1RecipeBox, renderRecipeTagsAll } from './function/render-recipebox.js';
+import { addRecipeToMyCollection } from './function/cart-and-collection.js';
+
+const groupDishes = [
+    {
+        name: 'Gỏi',
+        note: 'Các loại rau củ tươi trộn với nước sốt',
+        image: '/assets/groups/group-of-dishes-goi.jpg',
+    },
+    {
+        name: 'Kho',
+        note: 'Nấu với nước mắm cho vị mặn mà',
+        image: '/assets/groups/group-of-dishes-kho.jpg',
+    },
+    {
+        name: 'Cơm',
+        note: 'Tạo nên từ hạt ngọc của trời',
+        image: '/assets/groups/group-of-dishes-com.jpg',
+    },
+    {
+        name: 'Súp',
+        note: 'Nước trong lành ngon ngọt từ rau củ',
+        image: '/assets/groups/group-of-dishes-sup.jpg',
+    },
+    {
+        name: 'Chiên',
+        note: 'Từ dầu sôi lửa bỏng nên món ngon',
+        image: '/assets/groups/group-of-dishes-chien.jpg',
+    },
+    {
+        name: 'Nướng',
+        note: 'Điều kỳ diệu được tạo nên từ ngọn lửa',
+        image: '/assets/groups/group-of-dishes-nuong.jpg',
+    },
+    {
+        name: 'Lẩu',
+        note: 'Hòa quyện mọi thứ trong nồi lẩu nóng',
+        image: '/assets/groups/group-of-dishes-lau.jpg',
+    },
+]
 
 // Lấy số ngẫu nhiên
 function getRandomNumbers(arr, n) {
@@ -33,6 +72,74 @@ function renderSuggestRecipeAll() {
 renderSuggestRecipeAll();
 renderRecipeTagsAll();
 addRecipeToMyCollection();
+
+// Render 1 ô group món ăn
+function render1GroupBox(group) {
+    return `
+    <div class="group-block">
+        <a href="#">
+            <div class="group-core">
+                <div class="group-thumbnail">
+                    <img src="${group.image}" alt="${group.name}" />
+                </div>
+                <div class="group-content">
+                    <div class="group__name">
+                        <h3>${group.name} <i class="caret fa-solid fa-angle-down fa-rotate-270"></i></h3>
+                    </div>
+                    <div class="group__description">
+                        <p>${group.note}</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    `
+}
+// Render tất cả nhóm món ăn
+function renderGroupDishesAll() {
+    const parent = document.querySelector('.group-of-dishes-block .group-of-dishes__body');
+    const childs = groupDishes.map((group) => render1GroupBox(group));
+    parent.innerHTML = childs.reduce((string, item) => string + item, '');
+}
+renderGroupDishesAll();
+
+
+// Render 1 khung hiển thị đầu bếp
+function render1ChefBox(chef) {
+    return `
+    <div class="the-chef__wrap">
+        <div class="the-chef chef-${chef.chefId}">
+            <a href="#">
+                <div class="top">
+                    <div class="avatar">
+                        <img src="${chef.chefAvatar}" alt="${chef.chefName}" />
+                    </div>
+                    <div class="name-and-job">
+                        <h3>${chef.chefName}</h3>
+                        <h4>${chef.job}</h4>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <p>
+                        “${chef.quote}”
+                    </p>
+                </div>
+                <button class="see-more">Xem thêm</button>
+            </a>
+        </div>
+    </div>                        
+    `;
+}
+
+// Render chef-space 
+function renderChefAll() {
+    const parent = document.querySelector('.chef-space-block .chef-space__body .chef-space__core');
+    const childs = chefsList.map((chef) => render1ChefBox(chef));
+    parent.innerHTML = childs.reduce((string, item) => string + item, '');
+}
+
+renderChefAll();
+
 
 const nextChefBtn = document.getElementById('next-chef');
 const backChefBtn = document.getElementById('back-chef');
@@ -72,3 +179,5 @@ function transformChef(clickCount) {
 
 transformChef(0);
 makeBtnChefSlides();
+
+
